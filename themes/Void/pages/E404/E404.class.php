605d444a3910157d355930a4a8c44fee889f5007
Adrien Leloup <contact@void.graphics>
2016-03-22 17:42:30 +0100

    Fixed custom 404

---
+++ themes/Void/pages/E404/E404.class.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Theme\Void\Pages;
+
+use Kabas\Controller\BaseController;
+use Kabas\App;
+
+class E404 extends BaseController
+{
+}
---
+++ themes/Void/pages/E404/E404.php
@@ -0,0 +1,8 @@
+<?php Part::header(); ?>
+<h2><?= $title; ?></h2>
+<p>
+      <?= $errorMessage; ?>
+</p>
+
+
+<?php Part::footer(); ?>

bfcdbd5b9939c61e03acbae5c7cf4641f8cb4b49
Adrien Leloup <contact@void.graphics>
2016-03-22 10:56:24 +0100

    Added request type handling example

--- themes/Void/pages/homepage/home.class.php
+++ themes/Void/pages/homepage/home.class.php
@@ -3,8 +3,15 @@
 namespace Theme\Void\Pages;
 
 use Kabas\Controller\BaseController;
+use Kabas\App;
 
 class Homepage extends BaseController
 {
       // protected $view = 'home.php';
+      protected function setup()
+      {
+            if(App::request()->isPost()) {
+                  // Do stuff
+            }
+      }
 }

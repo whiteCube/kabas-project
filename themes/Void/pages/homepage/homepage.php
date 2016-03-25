823f2a5963de62e81076c1e5f14e42d041eb9cfe
Adrien Leloup <contact@void.graphics>
2016-03-25 16:32:32 +0100

    Added image handling

--- themes/Void/pages/homepage/homepage.php
+++ themes/Void/pages/homepage/homepage.php
@@ -3,16 +3,29 @@
 <p>
       <?= $intro ?>
 </p>
-<?php foreach($faveFoods->all() as $checkbox): ?>
-      <p>
-            <strong><?= $checkbox->isSelected() ? '●' : '○' ?> <?= $checkbox->label() ?></strong>
-      </p>
-<?php endforeach; ?>
 
-<?php if($isAwesome->isSelected()): ?>
-      <h2>Awesome mode activated!</h2>
-      <img src="https://media.giphy.com/media/DpB9NBjny7jF1pd0yt2/giphy.gif" alt="Awesome mode" />
-<?php endif; ?>
+<div>
+      <h2>Images</h2>
+      <a href="<?= Image::src($banner) ?>">
+      <img src="<?= Image::src($banner->fit(50, 100)->apply()) ?>" alt="" />
+      </a>
+</div>
+
+<div>
+      <h2>Checkboxes</h2>
+      <?php foreach($faveFoods->all() as $checkbox): ?>
+            <p>
+                  <strong><?= $checkbox->isSelected() ? '●' : '○' ?> <?= $checkbox->label() ?></strong>
+            </p>
+      <?php endforeach; ?>
+
+      <?php if($isAwesome->isSelected()): ?>
+            <h2>Awesome mode activated!</h2>
+            <img src="https://media.giphy.com/media/DpB9NBjny7jF1pd0yt2/giphy.gif" alt="Awesome mode" />
+      <?php endif; ?>
+</div>
+
+
 
 <?php Part::get('section1'); ?>
 

41a794f1d7d001a1e19acfcc62ece7c62f1733e4
Adrien Leloup <contact@void.graphics>
2016-03-30 11:33:18 +0200

    Optimized part loading - only load the required ones instead of all

--- themes/Void/pages/homepage/homepage.php
+++ themes/Void/pages/homepage/homepage.php
@@ -6,10 +6,16 @@
       <?= $intro ?>
 </p>
 
-<?php Part::get('colors'); ?>
+
 <?php Part::get('links'); ?>
 <?php Part::get('images'); ?>
 <?php Part::get('checkboxes'); ?>
 <?php Part::get('section1'); ?>
 
 <?php Part::footer(); ?>
+
+<?php use \Kabas\App;
+
+var_dump(App::config()->parts);
+
+ ?>

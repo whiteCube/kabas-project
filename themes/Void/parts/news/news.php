9821735cec75e801c2d40deadf4a5f4d19b114a6
Adrien Leloup <contact@void.graphics>
2016-03-31 17:44:19 +0200

    Generate instances of FieldTypes on Eloquent results

--- themes/Void/parts/news/news.php
+++ themes/Void/parts/news/news.php
@@ -2,7 +2,7 @@
       <h2><?= $title ?></h2>
       <?php foreach($news as $news_article): ?>
             <article>
-                  <h3><?= $news_article->title ?></h3>
+                  <h3><?= $news_article->title->uppercase() ?></h3>
                   <p><?= $news_article->content ?></p>
             </article>
       <?php endforeach; ?>

baf717d8ad1ae4a858346d0b66c935614f0ecc49
Adrien Leloup <contact@void.graphics>
2016-04-04 10:06:11 +0200

    Moved database configuration to proper file

--- config/database.php
+++ config/database.php
@@ -1,8 +1,12 @@
 <?php
 
 return [
-      "host" => "",
-      "user" => "",
-      "password" => "",
-      "database" => ""
+      'driver'    => 'mysql',
+      'host'      => 'localhost',
+      'database'  => 'kabas',
+      'username'  => 'root',
+      'password'  => 'root',
+      'charset'   => 'utf8',
+      'collation' => 'utf8_unicode_ci',
+      'prefix'    => '',
 ];

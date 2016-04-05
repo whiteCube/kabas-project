9acf4ff1db9d7d1b40d94f12d39b2ef8f170c076
Adrien Leloup <contact@void.graphics>
2016-04-05 14:22:33 +0200

    Cleanup

--- themes/Void/parts/news/news.class.php
+++ themes/Void/parts/news/news.class.php
@@ -10,10 +10,6 @@ class News extends BaseController
 {
       public function setup()
       {
-            $lol = NewsModel::find(4);
-            $lol->title = 'News number 4!';
-            $lol->save();
-
             $news = NewsModel::orderBy('id', 'desc')->limit(2)->get();
             $this->data->news = $news;
       }

54a1dabeed5a02ad63c10c21e96ed217c48f6be9
Adrien Leloup <contact@void.graphics>
2016-04-07 13:54:02 +0200

    Added code to post a news

--- themes/Void/pages/news/news.class.php
+++ themes/Void/pages/news/news.class.php
@@ -2,14 +2,31 @@
 
 namespace Theme\Void\Pages;
 
+use Kabas\App;
 use Kabas\Controller\BaseController;
 use Theme\Void\Models\News as NewsModel;
-use Kabas\App;
 
 class News extends BaseController
 {
       protected function setup()
       {
+
+            if(App::request()->isPost()){
+                  $this->saveNews();
+            }
+
             $this->data->news = NewsModel::orderBy('id', 'desc')->get();
+      }
+
+      public function saveNews()
+      {
+            $news = new NewsModel;
+            $news->title = App::request()->post->title;
+            $news->content = App::request()->post->content;
+            $news->image = [
+                  'src' => App::request()->post->imageSrc,
+                  'alt' => App::request()->post->imageAlt
+            ];
+            $news->save();
       }
 }

5d4cc854e67b086708ebd93e71ff7145725fcfbd
Adrien Leloup <contact@void.graphics>
2016-04-12 17:29:49 +0200

    Added example of url parsing

--- themes/Void/parts/links/links.php
+++ themes/Void/parts/links/links.php
@@ -1,4 +1,8 @@
 <div class="links">
       <h2>Links</h2>
       <a href="<?= $link1 ?>" title="<?= $link1->title ?>" <?= $link1->isLocal() ? '' : 'target="blank"' ?>><?= $link1->label ?></a>
+      <p>
+            Parsing:
+      </p>
+      <?php var_dump($link1->parse()); ?>
 </div>

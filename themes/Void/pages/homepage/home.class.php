bfd56873b01d03a80da8554133779399ca68bcff
Adrien Leloup <contact@void.graphics>
2016-04-13 18:10:22 +0200

    Started Session

--- themes/Void/pages/homepage/home.class.php
+++ themes/Void/pages/homepage/home.class.php
@@ -4,12 +4,17 @@ namespace Theme\Void\Pages;
 
 use Kabas\Controller\BaseController;
 use Kabas\App;
+use Kabas\Utils\Session;
 
 class Homepage extends BaseController
 {
       // protected $view = 'home.php';
       protected function setup()
       {
+
+            Session::put('test', 'testvalue');
+            var_dump(Session::get('test'));
+
             if(App::request()->isPost()) {
                   // Do stuff
             }

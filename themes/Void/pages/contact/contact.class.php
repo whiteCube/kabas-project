2c489883980e6b3dfb99c58ef3d7c0bed12d1a4d
Adrien Leloup <contact@void.graphics>
2016-04-14 16:16:39 +0200

    Session debugging

--- themes/Void/pages/contact/contact.class.php
+++ themes/Void/pages/contact/contact.class.php
@@ -3,8 +3,14 @@
 namespace Theme\Void\Pages;
 
 use Kabas\Controller\BaseController;
+use Kabas\Utils\Session;
 
 class Contact extends BaseController
 {
-
+      public function setup()
+      {
+            if(Session::hasFlash()){
+                  var_dump(Session::getFlash('flash'));
+            }
+      }
 }
--- themes/Void/pages/homepage/homepage.php
+++ themes/Void/pages/homepage/homepage.php
@@ -5,8 +5,8 @@
       <?= $intro->baseHeading(5) ?>
 </p>
 
-<?php Part::get('news'); ?>
-<?php Part::get('dates'); ?>
+<?php Part::get('news', ['title' => 'lol']); ?>
+<?php Part::dates(['title' => 'test']); ?>
 <?php Part::get('colors'); ?>
 <?php Part::get('links'); ?>
 <?php Part::get('images'); ?>

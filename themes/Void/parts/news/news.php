c0f7d61d1a767d8f9788db59aa4d6609bdd4e292
Adrien Leloup <contact@void.graphics>
2016-04-19 10:52:49 +0200

    Added touch method to JSON driver and updated_at fields

--- themes/Void/parts/news/news.php
+++ themes/Void/parts/news/news.php
@@ -4,8 +4,11 @@
             <article>
                   <h3><a href="/news/<?= $news_article->slug ?>"><?= $news_article->title->uppercase() ?></a></h3>
                   <p>
-                        <?= $news_article->created_at->formatLocalized('%A %d %B %Y') ?> (<?= $news_article->created_at->diffForHumans() ?>)
+                        Publié <?= $news_article->created_at->formatLocalized('%A %d %B %Y') ?> (<?= $news_article->created_at->diffForHumans() ?>)
                   </p>
+                  <small>
+                        Mis à jour <?= $news_article->updated_at->diffForHumans() ?>
+                  </small>
                   <p><?= $news_article->content ?></p>
                   <img src="<?= $news_article->image->fit(450, 300)->apply()->src() ?>" alt="<?= $news_article->image->alt() ?>" />
             </article>

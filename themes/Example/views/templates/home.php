b28ec0c2423978df2cc94ec5340d543c1fb71a7b
Toon Van den Bos <toon@whitecube.be>
2016-07-16 17:08:39 +0200

    Moved core Content Containers (Menus, Partials, Pages) to Content namespace

---
+++ themes/Example/views/templates/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <title>Test</title>
+</head>
+<body>
+      <h1>Test</h1>
+      <p>This is for v0.1.1 architectural reform testing purpose.</p>
+</body>
+</html>

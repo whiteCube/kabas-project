beb4f5afb9143d34676095fdebe23ddafe8cd0f0
Toon Van den Bos <toon@whitecube.be>
2016-07-22 10:14:35 +0200

    Updated Example theme homepage content (no dynamic content yet) - Added base css

--- themes/Example/views/templates/home.php
+++ themes/Example/views/templates/home.php
@@ -3,10 +3,29 @@
 <head>
       <meta charset="UTF-8">
       <title>Test</title>
+      <?php Assets::here('header'); ?>
+      <?php Assets::add('header', 'css/example.css'); ?>
 </head>
 <body>
-      <h1>Test</h1>
-      <p>This is for v0.1.1 architectural reform testing purpose.</p>
-      <p><?= $test->uppercase();?></p>
+      <div class="intro">
+            <header class="intro__head">
+                  <h1 class="intro__logo">Kabas</h1>
+                  <p class="intro__basline">Thought for humans, built for developers.</p>
+            </header>
+            <main class="intro__content">
+                  <section class="welcome">
+                        <h2 class="welcome__title">Great to see you!</h2>
+                        <p>This is the default Kabas theme. It's only purpose is to show you all the possibilities. Ready? Go.</p>
+                  </section>
+                  <nav class="next">
+                        <h2 class="next__title">What's next?</h2>
+                        <p>Let us help you during the first steps on this Kabas website.</p>
+                        <a href="#" class="next__item">Test</a>
+                  </nav>
+            </main>
+            <footer class="intro__end">
+                  <p class="intro__version">Kabas version <?= \Kabas\App::VERSION ?></p>
+            </footer>
+      </div>
 </body>
 </html>

6302413e2a79564c9164cb3a0c6f5f191f974ae7
Toon Van den Bos <toon@whitecube.be>
2016-07-22 12:17:09 +0200

    Added base example page css styling (little bit more)

--- themes/Example/views/templates/home.php
+++ themes/Example/views/templates/home.php
@@ -5,26 +5,24 @@
       <title>Test</title>
       <?php Assets::here('header', 'css/example.css'); ?>
 </head>
-<body>
-      <div class="intro">
-            <header class="intro__head">
-                  <h1 class="intro__logo">Kabas</h1>
-                  <p class="intro__basline">Thought for humans, built for developers.</p>
-            </header>
-            <main class="intro__content">
-                  <section class="welcome">
-                        <h2 class="welcome__title">Great to see you!</h2>
-                        <p>This is the default Kabas theme. It's only purpose is to show you all the possibilities. Ready? Go.</p>
-                  </section>
-                  <nav class="next">
-                        <h2 class="next__title">What's next?</h2>
-                        <p>Let us help you during the first steps on this Kabas website.</p>
-                        <a href="#" class="next__item">Test</a>
-                  </nav>
-            </main>
-            <footer class="intro__end">
-                  <p class="intro__version">Kabas version <?= \Kabas\App::VERSION ?></p>
-            </footer>
-      </div>
-</body>
+<body><div class="intro">
+      <header class="intro__head">
+            <h1 class="intro__logo">Kabas</h1>
+            <p class="intro__baseline">Thought for humans, built for developers.</p>
+      </header>
+      <main class="intro__content">
+            <section class="welcome">
+                  <h2 class="welcome__title">Great to see you!</h2>
+                  <p>This is the default Kabas theme. It's only purpose is to show you all the possibilities. Ready? Go.</p>
+            </section>
+            <nav class="next">
+                  <h2 class="next__title">What's next?</h2>
+                  <p>Let us help you during the first steps on this Kabas website.</p>
+                  <a href="#" class="next__item">Test</a>
+            </nav>
+      </main>
+      <footer class="intro__end">
+            <p class="intro__version">version <?= \Kabas\App::VERSION ?></p>
+      </footer>
+</div></body>
 </html>

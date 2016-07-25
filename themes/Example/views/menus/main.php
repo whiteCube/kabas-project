58427abb1129baceb095cda0dfe88d1b729862ba
Toon Van den Bos <toon@whitecube.be>
2016-07-25 16:28:00 +0200

    Added isActive() test on menuItems

--- themes/Example/views/menus/main.php
+++ themes/Example/views/menus/main.php
@@ -2,7 +2,7 @@
       <h2 class="next__title"><?= $title;?></h2>
       <div class="next__content"><?= $content;?></div>
       <?php foreach($items as $item):?>
-            <a href="<?= $item->url;?>" class="next__item<?= $item->icon ? ' next__item--' . $item->icon : '';?>">
+            <a href="<?= $item->url;?>" class="next__item<?= ($item->icon ? ' next__item--' . $item->icon : '') . ($item->isActive() ? ' next__item--active' : '');?>">
                   <span class="next__label"><?= $item->label;?></span>
             </a>
       <?php endforeach;?>

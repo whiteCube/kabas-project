9c64d569f952b52205e0aeda4d1ec95d342e9a8e
Toon Van den Bos <toon@whitecube.be>
2017-06-02 15:25:36 +0200

    Updated lang config

--- config/lang.php
+++ config/lang.php
@@ -4,13 +4,22 @@ return [
       /**
        * The languages available on the website.
        * Each available language must have its own folder inside the content folder.
+       * Should be a a valid W3C locale (e.g.: fr || en-GB || zh-Hant-SG).
+       * It is also possible to provide or override the default locale strings
+       * by providing a configuration array containing one or some of the following keys:
+       * 'my' => ['slug' => 'my-slug', 'label' => 'My Language', 'native' => 'Autonym', 'php' => 'my_PHP_locale']
        */
       'available' => [
-            'en-GB' => ['slug' => 'en', 'label' => 'English']
+            'en-GB'
       ],
 
       /**
        * Language used when the user's choice is undefined
        */
       'default' => 'en-GB',
+
+      /**
+       * Hide default language in generated URLs ?
+       */
+      'hideDefault' => true,
 ];

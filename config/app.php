c882a24d671621395f2ee0c4b18b878516ed5bab
Adrien Leloup <adrien@whitecube.be>
2018-10-18 13:13:40 +0200

    Updated base config

--- config/app.php
+++ config/app.php
@@ -12,59 +12,68 @@
 
 return [
 
-      /**
-       * Defines the errors display. When set to true, A customized Whoops
-       * will be used to display dumps and exceptions. If set to false, the default
-       * public error pages will be shown. You can make your own error pages.
-       * @var bool
-       */
-      "debug" => true,
+    /**
+    * Defines the errors display. When set to true, A customized Whoops
+    * will be used to display dumps and exceptions. If set to false, the default
+    * public error pages will be shown. You can make your own error pages.
+    * @var bool
+    */
+    "debug" => true,
 
-      /**
-       * The default driver used to get your site's data in the content
-       * folders. Only JSON is supported at the moment.
-       * @var string
-       */
-      'driver' => 'json',
+    /**
+    * The default driver used to get your site's data in the content
+    * folders. Only JSON is supported at the moment.
+    * @var string
+    */
+    'driver' => 'json',
 
-      /**
-       * The driver used by Intervention Image (gd | imagick)
-       */
-      'imageDriver' => 'gd',
+    /**
+    * The driver used by Intervention Image (gd | imagick)
+    */
+    'imageDriver' => 'gd',
 
-      /**
-       * The templating engine to use (available: 'php', 'blade')
-       * @var string
-       */
-      'templating' => 'blade',
+    /**
+    * The templating engine to use (available: 'php', 'blade')
+    * @var string
+    */
+    'templating' => 'blade',
 
-      /**
-       * Aliases for classes
-       * @var array
-       */
-      'aliases' => [
-            'Assets' => Kabas\Utils\Assets::class,
-            'Auth' => Kabas\Utils\Auth::class,
-            'Benchmark' => Kabas\Utils\Benchmark::class,
-            'Debug' => Kabas\Utils\Debug::class,
-            'File' => Kabas\Utils\File::class,
-            'Lang' => Kabas\Utils\Lang::class,
-            'Log' => Kabas\Utils\Log::class,
-            'Menu' => Kabas\Utils\Menu::class,
-            'Meta' => Kabas\Utils\Meta::class,
-            'Page' => Kabas\Utils\Page::class,
-            'Part' => Kabas\Utils\Part::class,
-            'Session' => Kabas\Utils\Session::class,
-            'Text' => Kabas\Utils\Text::class,
-            'Uploads' => Kabas\Utils\Uploads::class,
-            'Url' => Kabas\Utils\Url::class
-      ],
+    /**
+    * Aliases for classes
+    * @var array
+    */
+    'aliases' => [
+        'Assets' => Kabas\Utils\Assets::class,
+        'Auth' => Kabas\Utils\Auth::class,
+        'Benchmark' => Kabas\Utils\Benchmark::class,
+        'Debug' => Kabas\Utils\Debug::class,
+        'File' => Kabas\Utils\File::class,
+        'Lang' => Kabas\Utils\Lang::class,
+        'Log' => Kabas\Utils\Log::class,
+        'Menu' => Kabas\Utils\Menu::class,
+        'Meta' => Kabas\Utils\Meta::class,
+        'Page' => Kabas\Utils\Page::class,
+        'Part' => Kabas\Utils\Part::class,
+        'Session' => Kabas\Utils\Session::class,
+        'Text' => Kabas\Utils\Text::class,
+        'Uploads' => Kabas\Utils\Uploads::class,
+        'Url' => Kabas\Utils\Url::class
+    ],
 
-      /**
-       * List of service providers to load into the app
-       * @var array
-       */
-      'providers' => [
-            Kabas\Providers\View\ViewServiceProvider::class
-      ]
+    /**
+    * List of service providers to load into the app
+    * @var array
+    */
+    'providers' => [
+        Kabas\Providers\View\ViewServiceProvider::class
+    ],
+
+    /**
+     * Paths used by the blade rendering engine
+     * @var array
+     */
+    'views' => [
+        'sources' => ['views'],
+        'compiled' => 'storage/views/compiled'
+    ]
 ];
